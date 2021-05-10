c7db3d103e653dc9fd3c2c2dafe1836fe12beaba
MarioSotoF <sot18351@uvg.edu.gt>
2021-05-09 23:34:52 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/debug.h"
+#include "driverlib/gpio.h"
+volatile uint32_t i;
+
+
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+	return 0;
+}
