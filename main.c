509c1cee0d822b546f21c891c6ed61f6e5f2af21
MarioSotoF <sot18351@uvg.edu.gt>
2021-05-10 00:55:29 -0600

    configuracion del setup
    configuramos  el setup de pines como salidas y del boton como entrada, ademas se configura al boton como pullup

--- main.c
+++ main.c
@@ -22,10 +22,13 @@ volatile uint32_t i;
 
 int main(void)
 {
+
     SysCtlClockSet ( SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ ) ;//Hacemos la cofig del clck
     SysCtlPeripheralEnable ( SYSCTL_PERIPH_GPIOF ) ;//Activamos el uso de perierios para el puerto F
     // Setup de pines como salidas y de botones como entradas
-
-
+    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4);
+    //Configuramos el boton como pullup
+    GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
 
 }
