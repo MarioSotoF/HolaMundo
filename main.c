dbfb327d63f94462aba502ff1464bd241860089f
MarioSotoF <sot18351@uvg.edu.gt>
2021-05-10 01:34:25 -0600

    semaforo con mascaras
    codigo terminado y semaforo funcionando

--- main.c
+++ main.c
@@ -31,4 +31,43 @@ int main(void)
     //Configuramos el boton como pullup
     GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
 
+    while(1)
+        {
+            if (!GPIOPinRead(GPIO_PORTF_BASE,GPIO_PIN_4)){//Semaforo
+
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x02); //Se enciende el led rojo
+                for (i=0;i<5000000;i++){}
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x00); //Se apagan todas las leds
+                for (i=0;i<1000000;i++){}
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x0A); //Se enciende los leds rojo y verde para crear amarillo
+                for (i=0;i<5000000;i++){}
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x00); //Se apagan todas las leds
+                for (i=0;i<1000000;i++){}
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x08); //Se enciende la led verde
+                for (i=0;i<5000000;i++){}
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x00); //Se apagan todas las leds
+                for (i=0;i<1000000;i++){}
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x08); //Se inicia la secuencia de parpadeo para el verde
+                for (i=0;i<1000000;i++){}
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x00); //Se apagan todas las leds
+                for (i=0;i<1000000;i++){}
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x08); //Se enciende la led verde
+                for (i=0;i<1000000;i++){}
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x00); //Se apagan todas las leds
+                for (i=0;i<1000000;i++){}
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x08); //Se enciende la led verde
+                for (i=0;i<1000000;i++){}
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x00); //Se apagan todas las leds
+                for (i=0;i<1000000;i++){}
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x08); //Se enciende la led verde
+                for (i=0;i<1000000;i++){}
+                GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x00); //Se apagan todas las leds
+                for (i=0;i<1000000;i++){}
+
+                }
+
+        }
+
 }
+
+
