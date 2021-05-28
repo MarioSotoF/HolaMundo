0baac2fd4bdb1b6acaf816876fbc08c1eb03285b
MarioSotoF <sot18351@uvg.edu.gt>
2021-05-27 22:18:12 -0600

    7 segmentos
    7 segmentos ya indica cuantos espacios hay libres cuando se apachan los push.

--- main.c
+++ main.c
@@ -1,41 +1,41 @@
 /*
  * Nombre: Mario Soto
  * Carnet: 18351
- * Proyecto 4 parte de code composer
+ * Laboratorio 7
  * Catedratico: Pablo Mazariegos
  * Seccion 20
  */
 
 //Agregamos las librerias necesarias
 
-#include <stdint.h>
-#include <stdbool.h>
-#include "inc/tm4c123gh6pm.h"
-#include "inc/hw_memmap.h"
-#include "inc/hw_types.h"
-#include "inc/hw_ints.h"
-#include "driverlib/sysctl.h"
-#include "driverlib/interrupt.h"
-#include "driverlib/gpio.h"
-#include "driverlib/timer.h"
-#include "driverlib/systick.h"
+//Bibliotecas
+#include<stdint.h>
+#include<stdbool.h>
+#include"inc/hw_memmap.h"
+#include"inc/hw_types.h"
+#include"inc/tm4c123gh6pm.h"
+#include"driverlib/sysctl.h"
+#include"driverlib/gpio.h"
+#include"driverlib/timer.h"
+#include"driverlib/interrupt.h"
 #include "driverlib/uart.h"
 #include "driverlib/pin_map.h"
+#include "inc/hw_gpio.h"
 
 #define XTAL 16000000
 
 uint32_t ui32Period;//Se incluye la variable para el temporizador
 
-uint8_t parqueo1 = 0;
-uint8_t parqueo2 = 0;
-uint8_t parqueo3 = 0;
-uint8_t parqueo4 = 0;
-
-int32_t color;
+//lectura de los pines de los push
+int lectura;
+int lectura1;
+int lectura2;
+int lectura3;
+int libres;
 
 //void InitUART(void);
-//void Timer0IntHandler(void);
-void InitUART(void);
+
+
 
 
 
@@ -44,139 +44,160 @@ void InitUART(void);
  */
 int main(void)
 {
-    SysCtlClockSet ( SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ ) ;//Hacemos la cofig del clck
-    SysCtlPeripheralEnable ( SYSCTL_PERIPH_GPIOF|SYSCTL_PERIPH_GPIOD ) ;//Activamos el uso de periferios para el puerto F y D
+    SysCtlClockSet(SYSCTL_SYSDIV_5| SYSCTL_USE_PLL| SYSCTL_XTAL_16MHZ);
 
-    GPIOPinTypeGPIOInput(GPIO_PORTD_BASE, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3);//Configuramos los push buttoms como entradas
-
-    GPIOPinConfigure(GPIO_PD0_WT2CCP0);
-    GPIOPinConfigure(GPIO_PD1_WT2CCP1);
-    GPIOPinConfigure(GPIO_PD2_WT3CCP0);
-    GPIOPinConfigure(GPIO_PD3_WT3CCP1);
-
-    //Configuramos los push como pull up
-    GPIOPadConfigSet(GPIO_PORTD_BASE, GPIO_PIN_0, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
-    GPIOPadConfigSet(GPIO_PORTD_BASE, GPIO_PIN_1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
-    GPIOPadConfigSet(GPIO_PORTD_BASE, GPIO_PIN_2, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
-    GPIOPadConfigSet(GPIO_PORTD_BASE, GPIO_PIN_3, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+    SysCtlPeripheralEnable (SYSCTL_PERIPH_GPIOF) ;//Activamos el uso de periferios para el puerto F
+    SysCtlPeripheralEnable (SYSCTL_PERIPH_GPIOE) ;//Activamos el uso de periferios para el puerto E
+    SysCtlPeripheralEnable (SYSCTL_PERIPH_GPIOD) ;//Activamos el uso de periferios para el puerto D
+    SysCtlPeripheralEnable (SYSCTL_PERIPH_GPIOB) ;//Activamos el uso de periferios para el puerto B
+    SysCtlPeripheralEnable (SYSCTL_PERIPH_GPIOC) ;//Activamos el uso de periferios para el puerto C
+    SysCtlPeripheralEnable (SYSCTL_PERIPH_GPIOA) ;//Activamos el uso de periferios para el puerto C
 
     GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_1|GPIO_PIN_2);//Configuramos los dos leds del parqueo 1 como salidas
     GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_3|GPIO_PIN_4);//Configuramos los dos leds del parqueo 2 como salidas
     GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_6|GPIO_PIN_7);//Configuramos los dos leds del parqueo 3 como salidas
-    GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_6|GPIO_PIN_7);//Configuramos los dos leds del parqueo 4 como salidas
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1);//Configuramos los dos leds del parqueo 4 como salidas
-
-//    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);//Se habilita el reloj para el temporizador
-//    TimerConfigure(TIMER0_BASE, TIMER_CFG_PERIODIC);//Se configura al timer 0 como termporizador periodico
-//    ui32Period = (SysCtlClockGet()) / 2;//Se calcula el periodo del temporizador
-//    TimerLoadSet(TIMER0_BASE, TIMER_A, ui32Period - 1);//Se establece el periodo del temporizador
+    GPIOPinTypeGPIOOutput(GPIO_PORTC_BASE, GPIO_PIN_6|GPIO_PIN_7);//Configuramos los dos leds del parqueo 3 como salidas
 
 
-//    TimerIntEnable(TIMER0_BASE, TIMER_TIMA_TIMEOUT);//Se habilita el time out
-//    TimerIntRegister(TIMER0_BASE,TIMER_A,Timer0IntHandler);
-
-    InitUART();
-
-//    IntEnable(INT_TIMER0A);//Se habilita la interrupción en el timer 0
-//    IntMasterEnable();//Se habilitan interrupciones globales
-//    TimerEnable(TIMER0_BASE, TIMER_A);//Se habilita el timer
+    GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_4);//Configuramos el pin a del 7 segmentos como salida
+    GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_5|GPIO_PIN_6);//Configuramos el pin b y c del 7 segmentos como salidas
+    GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_7|GPIO_PIN_4);//Configuramos el pin d y e del 7 segmentos como salidas
+    GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_3|GPIO_PIN_2);//Configuramos el pin f ygc del 7 segmentos como salidas
 
 
-//    UARTCharPut(UART0_BASE, 'R');
-//    UARTCharPut(UART0_BASE, 'B');
-//    UARTCharPut(UART0_BASE, 'G');
+    GPIOPinTypeGPIOInput(GPIO_PORTE_BASE, GPIO_PIN_5);//Configuramos los push buttoms como entradas
+    GPIOPinTypeGPIOInput(GPIO_PORTD_BASE, GPIO_PIN_1);//Configuramos los push buttoms como entradas
+    GPIOPinTypeGPIOInput(GPIO_PORTD_BASE, GPIO_PIN_2);//Configuramos los push buttoms como entradas
+    GPIOPinTypeGPIOInput(GPIO_PORTD_BASE, GPIO_PIN_3);//Configuramos los push buttoms como entradas
+
+
+    GPIOPadConfigSet(GPIO_PORTE_BASE, GPIO_PIN_5, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);//Configuramos los push buttoms como pull up
+    GPIOPadConfigSet(GPIO_PORTD_BASE, GPIO_PIN_1, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);//Configuramos los push buttoms como pull up
+    GPIOPadConfigSet(GPIO_PORTD_BASE, GPIO_PIN_2, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);//Configuramos los push buttoms como pull up
+    GPIOPadConfigSet(GPIO_PORTD_BASE, GPIO_PIN_3, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);//Configuramos los push buttoms como pull up
+
+
+
+
+
+
 
     while (1)
     {
-//        color = UARTCharGet(UART0_BASE);
 
-        GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1, 0x02);
-        if (!GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_0))
+        lectura=GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_5);//Lectura para el parqueo 1
+        lectura1=GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_1);//Lectura para el parqueo 2
+        lectura2=GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_2);//Lectura para el parqueo 3
+        lectura3=GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_3);//Lectura para el parqueo 4
+//        libres = lectura + lectura1 + lectura2 + lectura3;
+        libres = 4;
+
+        if (lectura==0)
         {
-            SysCtlDelay(500000);
-            while (!GPIOPinRead(GPIO_PORTD_BASE, GPIO_PIN_0))
-            {
-
-            }
-
-            if (parqueo1 == 0)
-            {
-                GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1, 0xFF);//Led verde indicando que le parqueo esta libre
-                GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_2, 0x00);//Led roja apagada
-                SysCtlDelay(500000);
-                parqueo1 = 1;
-            }
-            else if(parqueo1 == 1)
-            {
-                GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1, 0x00);//Led verde apagada
-                GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_2, 0xFF);//Led roja indicando que le parqueo esta ocuapo
-                SysCtlDelay(500000);
-                parqueo1 = 0;
-            }
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1, 0);//Led verde apagada
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_2, GPIO_PIN_2);//Led roja encendida indicando que el parqueo esta ocupado
+            libres --;
         }
+        if (!lectura==0)
+        {
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1, GPIO_PIN_1);//Led verde encendida indicando que le parqueo esta libre
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_2, 0);//Led roja apagada
+        }
+        if(lectura1 == 0)
+        {
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_3, 0);//Led verde apagada
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_4, GPIO_PIN_4);//Led roja encendida indicando que el parqueo esta ocupado
+            libres --;
 
+        }
+        if(!lectura1 == 0)
+        {
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_3, GPIO_PIN_3);//Led verde encendida indicando que le parqueo esta libre
+            GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_4, 0);//Led roja apagada
+        }
+        if(lectura2 == 0)
+        {
+            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_6, 0);//Led verde apagada
+            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_7, GPIO_PIN_7);//Led roja encendida indicando que el parqueo esta ocupado
+            libres --;
+
+        }
+        if(!lectura2 == 0)
+        {
+            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_6, GPIO_PIN_6);//Led verde encendida indicando que le parqueo esta libre
+            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_7, 0);//Led roja apagada
+        }
+        if(lectura3 == 0)
+        {
+            GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6, 0);//Led verde apagada
+            GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_7, GPIO_PIN_7);//Led roja encendida indicando que el parqueo esta ocupado
+            libres --;
+
+        }
+        if(!lectura3 == 0)
+        {
+            GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_6, GPIO_PIN_6);//Led verde encendida indicando que le parqueo esta libre
+            GPIOPinWrite(GPIO_PORTC_BASE, GPIO_PIN_7, 0);//Led roja apagada
+        }
+        if(libres == 4)
+        {
+            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_4, 0);//Apagamos a
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_5, GPIO_PIN_5);//Encendemos b
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_6, GPIO_PIN_6);//Encendemos c
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_7, 0);//Apagamos d
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_4, 0);//Apagamos e
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_3, GPIO_PIN_3);//Encendemos f
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2, GPIO_PIN_2);//Encendemos g
+
+
+        }
+        if(libres == 3)
+        {
+            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_4, GPIO_PIN_4);//Encendemos a
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_5, GPIO_PIN_5);//Encendemos b
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_6, GPIO_PIN_6);//Encendemos c
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_7, GPIO_PIN_7);//Encendemos d
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_4, 0);//Apagamos e
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_3, 0);//Apagamos f
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2, GPIO_PIN_2);//Encendemos g
+        }
+        if(libres == 2)
+        {
+            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_4, GPIO_PIN_4);//Encendemos a
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_5, GPIO_PIN_5);//Encendemos b
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_6, 0);//Apagamos c
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_7, GPIO_PIN_7);//Encendemos d
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_4, GPIO_PIN_4);//Encendemos e
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_3, 0);//Apagamos f
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2, GPIO_PIN_2);//Encendemos g
+        }
+        if(libres == 1)
+        {
+            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_4, 0);//Apagamos a
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_5, GPIO_PIN_5);//Encendemos b
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_6, GPIO_PIN_6);//Encendemos c
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_7, 0);//Apagamos d
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_4, 0);//Apagamos e
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_3, 0);//Apagamos f
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2, 0);//Apagamos g
+        }
+        if(libres == 0)
+        {
+            GPIOPinWrite(GPIO_PORTB_BASE, GPIO_PIN_4, GPIO_PIN_4);//Encendemos a
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_5, GPIO_PIN_5);//Encendemos b
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_6, GPIO_PIN_6);//Encendemos c
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_7, GPIO_PIN_7);//Encendemos d
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_4, GPIO_PIN_4);//Encendemos e
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_3, GPIO_PIN_3);//Encendemos f
+            GPIOPinWrite(GPIO_PORTA_BASE, GPIO_PIN_2, 0);//Apagamos g
+        }
+        else
+        {
+
+        }
 
     }
 
 
 
 }
-
-
-//void Timer0IntHandler(void){
-//    TimerIntClear(TIMER0_BASE, TIMER_TIMA_TIMEOUT);//Se limpia la interrupcion del timer
-//
-//    switch (color)
-//    {
-//    case 'r':
-//        if (bandera == 0)
-//        {
-//            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x02); //Se enciende el led rojo
-//            bandera = 1;
-//        }
-//        else
-//        {
-//            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x00); //Se apagan todas las leds
-//            bandera = 0;
-//        }
-//        break;
-//    case 'b':
-//        if (bandera == 0)
-//        {
-//            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x04);
-//            bandera = 1;
-//        }
-//        else
-//        {
-//            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x00); //Se apagan todas las leds
-//            bandera = 0;
-//        }
-//        break;
-//    case 'g':
-//        if (bandera == 0)
-//        {
-//            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3,0x08);
-//            bandera = 1;
-//        }
-//       else
-//        {
-//            GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_3|GPIO_PIN_2|GPIO_PIN_1,0x00); //Se apagan todas las leds
-//            bandera = 0;
-//        }
-//        break;
-//    }
-//}
-
-void InitUART(void)
-{
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);//Habilitamos el puerto A del GPIO
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);//Se activa el periferio del Uart
-    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);//Hacemos que los pines de la comunicación Uart se puedean controlar de manera periferica
-    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
-    UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
-
-
-}
-
-
-
